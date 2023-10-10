9b75236e823bac67ce77d640b80eb8b98fb08462
enzoluizb <enzoluiz.brt@gmail.com>
2023-10-10 14:13:26 -0300

    lista de tarefas finalizada

--- fundamentos/List/script.js
+++ fundamentos/List/script.js
@@ -4,6 +4,24 @@ new Vue({
     return {
       titulo: "Minhas tarefas",
       tarefas: [{ titulo: "Estudar", checked: true }, { titulo: "Ler", checked: false }, { titulo: "Escrever", checked: false}],
+      novaTarefa: '',
     };
   },
+  methods: {
+    adicionarTarefa() {
+        this.tarefas.push({
+            titulo: this.novaTarefa, checked: false
+        })
+        this.novaTarefa = ''
+    }
+  },
+  computed: {
+    totalTarefas() {
+        return this.tarefas.length
+    },
+    totalTarefasConcluidas() {
+        const concluidas = this.tarefas.filter(tarefa => tarefa.checked)
+        return concluidas.length
+    }
+  }
 });
