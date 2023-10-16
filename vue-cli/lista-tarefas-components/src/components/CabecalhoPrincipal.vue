561075361ea4c58d10b4526023709a7021e7aee9
enzoluizb <enzoluiz.brt@gmail.com>
2023-10-16 13:38:34 -0300

    estilo dos components

--- vue-cli/lista-tarefas-components/src/components/CabecalhoPrincipal.vue
+++ vue-cli/lista-tarefas-components/src/components/CabecalhoPrincipal.vue
@@ -10,6 +10,12 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+footer {
+  background-color: lightcyan;
+  height: 50px;
+  width: 100%;
+  position: fixed;
+  bottom: 0px;
+}
 </style>
