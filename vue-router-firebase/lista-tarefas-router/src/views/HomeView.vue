400d6f92183736f0abe3a0e96fe56a4961b50b7c
enzoluizb <enzoluiz.brt@gmail.com>
2023-10-26 10:54:17 -0300

    profileCollection && tasksCollection

--- vue-router-firebase/lista-tarefas-router/src/views/HomeView.vue
+++ vue-router-firebase/lista-tarefas-router/src/views/HomeView.vue
@@ -1,42 +1,65 @@
 <template>
   <v-container class="pa-8" fluid>
-    <h1>Minha Lista de Tarefas</h1>
+    <h1 class="h1">Minha lista de tarefas</h1>
     <v-form>
-      <v-text-field label="Nova Tarefa" v-model="novaTarefa" @keydown.enter="adicionar">
+      <v-text-field
+        label="Nova tarefa"
+        v-model="novaTarefa"
+        @keydown.enter="adicionar"
+      >
         <v-icon slot="append" @click="adicionar">mdi-send</v-icon>
       </v-text-field>
     </v-form>
     <v-divider></v-divider>
     <v-list>
       <v-list-item-group>
-        <v-list-item v-for="tarefa of tarefas" :key="tarefa.titulo"> {{ tarefa.titulo }}</v-list-item>
+        <v-list-item v-for="tarefa of tarefas" :key="tarefa.id">
+          {{ tarefa.titulo }}
+        </v-list-item>
       </v-list-item-group>
     </v-list>
   </v-container>
 </template>
 
 <script>
+import * as fb from "@/plugins/firebase";
 export default {
   data() {
     return {
-      novaTarefa: '',
-      tarefas: [
-        {titulo: "Tarefa 1"},
-        {titulo: "Tarefa 1"},
-        {titulo: "Tarefa 1"},
-      ]
+      uid: "",
+      novaTarefa: "",
+      tarefas: [],
     };
   },
+  mounted() {
+    this.uid = fb.auth.currentUser.uid;
+    this.buscarTarefasDoServidor();
+  },
   methods: {
-    adicionar(e) {
-      e.preventDefault();
-      this.tarefas.push({titulo: this.novaTarefa})
-      this.novaTarefa = ""
-    }
-  }
-}
+    async buscarTarefasDoServidor() {
+      this.tarefas = [];
+      const logTasks = await fb.tasksCollection
+        .where("owner", "==", this.uid)
+        .get();
+      for (const doc of logTasks.docs) {
+        this.tarefas.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+        });
+      }
+    },
+    async adicionar() {
+      await fb.tasksCollection.add({
+        titulo: this.novaTarefa,
+        dataGravacao: new Date().toISOString().slice(0, 10),
+        owner: this.uid,
+      });
+      this.novaTarefa = "";
+      this.buscarTarefasDoServidor();
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
